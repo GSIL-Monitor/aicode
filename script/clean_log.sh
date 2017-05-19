eb6053c82acd776862af08699e519c6a461ae3c2
zxdyb <zxdyb@163.com>
2017-05-19 15:15:50 +0800

    增加清除日志脚本

---
+++ script/clean_log.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -lt 2 ]
+then
+    echo "usage: $0 path size";
+    exit;
+fi
+
+flag=1  
+result=1  
+while [ "$flag" -eq 1 ]  
+do  
+    sleep 1s  
+    result=`du -m $1|awk '{print $1}'`
+    if [ "$result" -gt $2 ]; then  
+        echo "begin clean $1"
+        dir="$1""/*"
+        rm -fr $dir
+        echo "end clean $1"  
+    fi  
+done
