eec22e0807752de8e33d10c33fd60b6bd96f194c
zxdyb <zxdyb@163.com>
2017-06-29 14:43:57 +0800

    修改更新脚本

--- script/update.sh
+++ script/update.sh
@@ -23,11 +23,40 @@ acc_update()
     rm -fr update_acc.py*
 }
 
+file_http_update()
+{
+    cp update_templete.py update_file_http.py
+    sed -i "s/_local_path/\/usr\/local\/src\/modules\/module\/file_http_server\/bin/g" update_file_http.py
+    sed -i "s/_local_file/filemgr.cgi/g" update_file_http.py
+    sed -i "s/_remote_path/\/data\/platform\/filemgr\/bin/g" update_file_http.py
+    sed -i "s/_remote_run/runfile.sh/g" update_file_http.py
+    fab -f update_file_http.py start
+    rm -fr update_file_http.py*
+}
+
+file_handler_update()
+{
+    cp update_templete.py update_file_handler.py
+    sed -i "s/_local_path/\/usr\/local\/src\/modules\/module\/file_handler_server\/bin/g" update_file_handler.py
+    sed -i "s/_local_file/FileHandler/g" update_file_handler.py
+    sed -i "s/_remote_path/\/data\/platform\/file_handler_server\/bin/g" update_file_handler.py
+    sed -i "s/_remote_run/run.sh/g" update_file_handler.py
+    fab -f update_file_handler.py start
+    rm -fr update_file_handler.py*
+}
+
+
 if [[ "http" = $1 ]]; then 
     http_update
     
 elif [[ "acc" = $1 ]]; then 
     acc_update
+    
+elif [[ "filehttp" = $1 ]]; then 
+    file_http_update
+    
+elif [[ "filehandler" = $1 ]]; then 
+    file_handler_update
     
 elif [[ "all" = $1 ]]; then 
     http_update
